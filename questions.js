6d69cb07302841cd29aaee443b50465c4673f20a
politicalideas <74066837+politicalideas@users.noreply.github.com>
2020-11-08 12:27:21 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -17,24 +17,5 @@ questions = [
             "state2": 0,
             "state3": 0,
         }
-    },
-    {
-        "question": "Ziemia nie powinna być obiektem do kupna i sprzedaży",
-        "effect": {
-            "econ1": 15,
-            "econ2": 0,
-            "econ3": 0,
-            "law1": 0,
-            "law2": 0,
-            "law3": 0,
-            "law4": 0,
-            "society1": 0,
-            "society2": 0,
-            "society3": 0,
-            "society4": 0,
-            "state1": 0,
-            "state2": 0,
-            "state3": 0,
-        }
     }
 ];
